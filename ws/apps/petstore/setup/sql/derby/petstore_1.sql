d399c5aca3bec87253535a1ec5c10d6ed364c5c7
smitha <smitha@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2005-11-30 23:03:41 +0000

    changed image URLs

--- ws/apps/petstore/setup/sql/derby/petstore_1.sql
+++ ws/apps/petstore/setup/sql/derby/petstore_1.sql
@@ -33,19 +33,19 @@ CREATE TABLE item_1 (
 INSERT INTO category_1 VALUES('CATS', 'Cats', 'Loving and furry friends', 'cats_icon.gif');
 INSERT INTO category_1 VALUES('DOGS','Dogs', 'Loving and furry friends', 'dogs_icon.gif');
 
-INSERT INTO product_1 VALUES('cat001', 'CATS', 'Manx', 'Great for reducing mouse populations', 'cat.gif');
-INSERT INTO product_1 VALUES('cat002', 'CATS', 'Persian', 'Friendly house cat', 'cat.gif');
-INSERT INTO product_1 VALUES('dog001', 'DOGS', 'Bull Dog', 'Friendly dog from England', 'dog.gif');
-INSERT INTO product_1 VALUES('dog002', 'DOGS', 'Poodle', 'Great Companion Dog','dog.gif');
+INSERT INTO product_1 VALUES('cat001', 'CATS', 'Manx', 'Great for reducing mouse populations', 'cat1.gif');
+INSERT INTO product_1 VALUES('cat002', 'CATS', 'Persian', 'Friendly house cat', 'cat2.gif');
+INSERT INTO product_1 VALUES('dog001', 'DOGS', 'Bull Dog', 'Friendly dog from England', 'dog1.gif');
+INSERT INTO product_1 VALUES('dog002', 'DOGS', 'Poodle', 'Great Companion Dog','dog2.gif');
 
 
-INSERT INTO item_1 VALUES('EST-1','dog001','Male Puppy','Friendly dog from England','dog.gif',2570,2000);
-INSERT INTO item_1 VALUES('EST-2','dog001','Female Puppy','Friendly dog from England','dog.gif',2590,2100);
-INSERT INTO item_1 VALUES('EST-3','dog002','Male Puppy','Great Companion Dog','dog.gif',2570,2000);
-INSERT INTO item_1 VALUES('EST-4','dog002','Female Puppy','Great Companion Dog','dog.gif',2590,2100);
-INSERT INTO item_1 VALUES('EST-5','cat001','Tailless','Great for reducing mouse populations','cat.gif',2570,2000);
-INSERT INTO item_1 VALUES('EST-6','cat001','With tail','Great for reducing mouse populations','cat.gif',2590,2100);
-INSERT INTO item_1 VALUES('EST-7','cat002','Adult Male','Friendly house cat','cat.gif',2570,2000);
-INSERT INTO item_1 VALUES('EST-8','cat002','Adult Female','Friendly house cat','cat.gif',2590,2100);
+INSERT INTO item_1 VALUES('EST-1','dog001','Male Puppy','Friendly dog from England','dog1.gif',2570,2000);
+INSERT INTO item_1 VALUES('EST-2','dog001','Female Puppy','Friendly dog from England','dog1.gif',2590,2100);
+INSERT INTO item_1 VALUES('EST-3','dog002','Male Puppy','Great Companion Dog','dog2.gif',2570,2000);
+INSERT INTO item_1 VALUES('EST-4','dog002','Female Puppy','Great Companion Dog','dog2.gif',2590,2100);
+INSERT INTO item_1 VALUES('EST-5','cat001','Tailless','Great for reducing mouse populations','cat1.gif',2570,2000);
+INSERT INTO item_1 VALUES('EST-6','cat001','With tail','Great for reducing mouse populations','cat1.gif',2590,2100);
+INSERT INTO item_1 VALUES('EST-7','cat002','Adult Male','Friendly house cat','cat2.gif',2570,2000);
+INSERT INTO item_1 VALUES('EST-8','cat002','Adult Female','Friendly house cat','cat2.gif',2590,2100);
 
 
