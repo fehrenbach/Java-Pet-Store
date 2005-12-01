59eb770819e4afb41a9a7339636b128f9154bf0f
gmurray71 <gmurray71@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2005-12-01 05:00:48 +0000

    Added Product.java Issue number: Obtained from: Submitted by: Reviewed by:

---
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/Product.java
@@ -0,0 +1,72 @@
+package com.sun.javaee.blueprints.petstore.model;
+        
+import javax.persistence.*;
+
+@Entity
+@Table(name="PRODUCT") 
+
+public class Product implements java.io.Serializable{
+    
+    private String productID;
+    private String categoryID;
+    private String name;
+    private String description;
+    private String imageURL;
+    
+    public Product(String productID,
+            String categoryID,
+            String name,
+            String description,
+            String imageURL) {
+        
+        this.productID = productID;
+        this.categoryID = categoryID;
+        this.name = name;
+        this.description =  description;
+        this.imageURL = imageURL;
+    }
+    
+    public Product() { }
+    
+    @Id
+    public String getProductID() {
+        return this.productID;
+    }
+    
+    public String getCategoryID() {
+        return categoryID;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }    
+    
+    public String getImageURL() {
+        return imageURL;
+    }
+    
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+     
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+ 
+}
+
+
+
