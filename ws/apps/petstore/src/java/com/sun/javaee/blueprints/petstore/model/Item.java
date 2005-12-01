7e9a4dc9da0272d2c2ba4fb68f2d530c142fc294
gmurray71 <gmurray71@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2005-12-01 05:54:12 +0000

    Made serializable bt it still does not work. Issue number: Obtained from: Submitted by: Reviewed by:

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/Item.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/Item.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 @Table(name="ITEM") 
 
-public class Item {
+public class Item implements java.io.Serializable {
 
     private String itemID;
     private String productID;
