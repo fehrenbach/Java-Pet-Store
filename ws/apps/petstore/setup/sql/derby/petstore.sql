8f50910b46f5f426a8df46ece3237af68154b2e2
smitha <smitha@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-03-08 05:38:03 +0000

    changed item IDs to be sequential so that it can be autogenerated

--- ws/apps/petstore/setup/sql/derby/petstore.sql
+++ ws/apps/petstore/setup/sql/derby/petstore.sql
@@ -68,20 +68,20 @@ INSERT INTO product VALUES('feline02', 'CATS', 'Short Hair', 'Friendly house cat
 INSERT INTO product VALUES('canine01', 'DOGS', 'Medium Dogs', 'Friendly dog from England', 'dog1.gif');
 INSERT INTO product VALUES('canine02', 'DOGS', 'Small Dogs', 'Great Companion Dog','dog2.gif');
 
-INSERT INTO item VALUES('pet-001', 'feline01', 'Lazy cat', 'A great pet', 'cat1.gif', 307, 290);
-INSERT INTO item VALUES('pet-002', 'feline01', 'Old Cat', 'A great old pet', 'cat2.gif', 207, 200);
-INSERT INTO item VALUES('pet-003', 'feline01', 'Young Cat', 'A great young pet', 'cat3.gif', 407, 350);
-INSERT INTO item VALUES('pet-004', 'feline01', 'Scrapper Cat', 'A scapper cat', 'cat4.gif', 417, 370);
-INSERT INTO item VALUES('pet-005', 'feline01', 'Alley Cat', 'A great pet', 'cat5.gif', 307, 290);
-INSERT INTO item VALUES('pet-006', 'feline02', 'Playful Cat', 'A needy pet', 'cat7.gif', 190, 150);
-INSERT INTO item VALUES('pet-007', 'feline02', 'Long Haired Cat', 'A high maintenance cat', 'cat8.gif', 199, 175);
-INSERT INTO item VALUES('pet-008', 'feline02', 'Smelly Cat', 'A great pet', 'cat9.gif', 303, 200);
-INSERT INTO item VALUES('pet-009', 'feline02', 'Wild Cat', 'A great wild pet', 'cat10.gif', 527, 240);
-INSERT INTO item VALUES('pet-010', 'feline02', 'Saber Cat', 'A great watch pet', 'cat11.gif', 237, 200);
-INSERT INTO item VALUES('pet-011', 'feline02',' Scrapper Cat', 'A scapper cat', 'cat12.gif', 337, 190);
+INSERT INTO item VALUES('1', 'feline01', 'Lazy cat', 'A great pet', 'cat1.gif', 307, 290);
+INSERT INTO item VALUES('2', 'feline01', 'Old Cat', 'A great old pet', 'cat2.gif', 207, 200);
+INSERT INTO item VALUES('3', 'feline01', 'Young Cat', 'A great young pet', 'cat3.gif', 407, 350);
+INSERT INTO item VALUES('4', 'feline01', 'Scrapper Cat', 'A scapper cat', 'cat4.gif', 417, 370);
+INSERT INTO item VALUES('5', 'feline01', 'Alley Cat', 'A great pet', 'cat5.gif', 307, 290);
+INSERT INTO item VALUES('6', 'feline02', 'Playful Cat', 'A needy pet', 'cat7.gif', 190, 150);
+INSERT INTO item VALUES('7', 'feline02', 'Long Haired Cat', 'A high maintenance cat', 'cat8.gif', 199, 175);
+INSERT INTO item VALUES('8', 'feline02', 'Smelly Cat', 'A great pet', 'cat9.gif', 303, 200);
+INSERT INTO item VALUES('9', 'feline02', 'Wild Cat', 'A great wild pet', 'cat10.gif', 527, 240);
+INSERT INTO item VALUES('10', 'feline02', 'Saber Cat', 'A great watch pet', 'cat11.gif', 237, 200);
+INSERT INTO item VALUES('11', 'feline02',' Scrapper Cat', 'A scapper cat', 'cat12.gif', 337, 190);
 
-INSERT INTO item VALUES('pet-012', 'canine01', 'Lazy Dog', 'A great pet', 'dog1.gif', 257, 201);
-INSERT INTO item VALUES('pet-013', 'canine01', 'Old Dog', 'A great old pet', 'dog2.gif', 70, 50);
-INSERT INTO item VALUES('pet-014', 'canine01', 'Young Dog', 'A great young pet', 'dog3.gif', 301, 210);
-INSERT INTO item VALUES('pet-015', 'canine02', 'Scrapper Dog', 'A scapper dog', 'dog4.gif', 410, 210);
-INSERT INTO item VALUES('pet-016', 'canine02', 'Grey Hound', 'A great runner', 'dog5.gif', 200, 210);
+INSERT INTO item VALUES('12', 'canine01', 'Lazy Dog', 'A great pet', 'dog1.gif', 257, 201);
+INSERT INTO item VALUES('13', 'canine01', 'Old Dog', 'A great old pet', 'dog2.gif', 70, 50);
+INSERT INTO item VALUES('14', 'canine01', 'Young Dog', 'A great young pet', 'dog3.gif', 301, 210);
+INSERT INTO item VALUES('15', 'canine02', 'Scrapper Dog', 'A scapper dog', 'dog4.gif', 410, 210);
+INSERT INTO item VALUES('16', 'canine02', 'Grey Hound', 'A great runner', 'dog5.gif', 200, 210);
