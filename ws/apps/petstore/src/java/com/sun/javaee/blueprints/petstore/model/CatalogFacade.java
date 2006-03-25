9adaa4c23330bffdca12954816aad1bedaf17f2a
smitha <smitha@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-03-25 20:58:31 +0000

    added a method to persist items

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/CatalogFacade.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/CatalogFacade.java
@@ -1,100 +1,214 @@
 package com.sun.javaee.blueprints.petstore.model;
 
+
+
 import java.util.*;
 
+
+
 import javax.servlet.*;
+
 import javax.ejb.*;
+
 import javax.persistence.*;
+import javax.transaction.*;
+import javax.annotation.*;
+
 
 @NamedQueries(
+
  {  @NamedQuery(
+
       name="getItemsPerProductCategory",
+
       query="SELECT i FROM Item i WHERE i.productID.categoryID LIKE :cID"
+
     ), 
+
     @NamedQuery(
+
       name="getAllZipCityState",
+
       query="SELECT  z FROM ZipLocation z"       
+
     )
+
   }
+
 ) 
+
 public class CatalogFacade implements ServletContextListener {
 
+
+
    @PersistenceContext(name="bppu")
+
    private EntityManager em;
+   
+   @Resource UserTransaction utx;
+
+
 
     public CatalogFacade(){
+
         System.out.println("Created Catalog Facade...Entity manager=" + em);
+
     }
+
     
+
     public void contextDestroyed(ServletContextEvent sce) {}
+
     
+
     public void contextInitialized(ServletContextEvent sce) {
+
         ServletContext context = sce.getServletContext();
+
         context.setAttribute("CatalogFacade", this);
+
     }
+
+
 
     public Collection getCategories(){
+
         return em.createNativeQuery("SELECT * FROM Category").getResultList();
-    }
-    
-    public Collection getAllItemsFromCategory(String catID){
-        return em.createNativeQuery("SELECT  * FROM Item, Product WHERE Item.productID = Product.productID AND Product.categoryID LIKE ?")
-        .setParameter(1, "%"+catID+"%").getResultList();
+
     }
 
-    /**
-     * Value List Handler for items. Uses the Java Persistence query language.
-     * @param catID is the category id that the item belongs to
-     * @param start position of the first result, numbered from 0
-     * @param chunkSize the maximum number of results to retrieve
-     * @returns a List of Item objects
-     */
-    public List<Item> getItemsVLH(String catID, int start, int chunkSize){     
-       //make Java Persistence query
-       //Query query = em.createQuery("SELECT  i FROM Item i, Product p WHERE i.productID = p.productID AND p.categoryID LIKE :cID")
-       //List<Item>  items = query.setParameter("cID",catID).setFirstResult(start).setMaxResults(chunkSize).getResultList();
-       Query query = em.createNamedQuery("getItemsPerProductCategory");
-       query.setParameter("cID",catID);
-       query.setFirstResult(start).setMaxResults(chunkSize);
-       List<Item> items = query.getResultList(); 
-       return items;
-    }
     
-    /**
-     * Gets a list of all the zipcode/city/state for autocomplte on user forms
-     * Need to enhance so that returned list is cached for reuse at application scope 
-     * and held as member field of facade.
-     * @returns a List of ZipLocation objects
-     */
-    public List<ZipLocation> getZipCodeLocations(){     
-       //Query query = em.createQuery("SELECT  z FROM ZipLocation z");
-       Query query = em.createNamedQuery("getAllZipCityState");
-       List<ZipLocation>  zipCodeLocations = query.getResultList();    
-       return zipCodeLocations;
+
+    public Collection getAllItemsFromCategory(String catID){
+
+        return em.createNativeQuery("SELECT  * FROM Item, Product WHERE Item.productID = Product.productID AND Product.categoryID LIKE ?")
+
+        .setParameter(1, "%"+catID+"%").getResultList();
+
     }
+
+
+
+    /**
+
+     * Value List Handler for items. Uses the Java Persistence query language.
+
+     * @param catID is the category id that the item belongs to
+
+     * @param start position of the first result, numbered from 0
+
+     * @param chunkSize the maximum number of results to retrieve
+
+     * @returns a List of Item objects
+
+     */
+
+    public List<Item> getItemsVLH(String catID, int start, int chunkSize){     
+
+       //make Java Persistence query
+
+       //Query query = em.createQuery("SELECT  i FROM Item i, Product p WHERE i.productID = p.productID AND p.categoryID LIKE :cID")
+
+       //List<Item>  items = query.setParameter("cID",catID).setFirstResult(start).setMaxResults(chunkSize).getResultList();
+
+       Query query = em.createNamedQuery("getItemsPerProductCategory");
+
+       query.setParameter("cID",catID);
+
+       query.setFirstResult(start).setMaxResults(chunkSize);
+
+       List<Item> items = query.getResultList(); 
+
+       return items;
+
+    }
+
+    
+
+    /**
+
+     * Gets a list of all the zipcode/city/state for autocomplte on user forms
+
+     * Need to enhance so that returned list is cached for reuse at application scope 
+
+     * and held as member field of facade.
+
+     * @returns a List of ZipLocation objects
+
+     */
+
+    public List<ZipLocation> getZipCodeLocations(){     
+
+       //Query query = em.createQuery("SELECT  z FROM ZipLocation z");
+
+       Query query = em.createNamedQuery("getAllZipCityState");
+
+       List<ZipLocation>  zipCodeLocations = query.getResultList();    
+
+       return zipCodeLocations;
+
+    }
+
+
 
     public Collection getProducts(String catID){
+
         return em.createNativeQuery("SELECT * FROM Product WHERE categoryID LIKE ?")
+
         .setParameter(1, "%"+catID+"%").getResultList();
+
     }
+
+
 
     public Collection getItems(String productID){
+
         return em.createNativeQuery("SELECT * FROM Item WHERE productid LIKE ?")
+
                 .setParameter(1, "%"+productID+"%").getResultList();
+
     }
+
+
 
     public Category getCategory(String categoryID){
+
         return em.find(Category.class,categoryID);
+
     }
 
+
+
     public Item getItem(String itemID){
+
         return em.find(Item.class,itemID);
+
+    }
+    
+    public void addItem(Item item){
+        try{
+            utx.begin();
+            em.persist(item);
+            utx.commit();
+        } catch(Exception exe){
+            System.out.println("Error persisting item: "+exe);
+            try {
+                utx.rollback();
+            } catch (Exception e) {}
+        } 
     }
 
     public Collection doSearch(String querryString){
+
         Query searchQuery = em.createNativeQuery("SELECT * FROM Item WHERE (name LIKE ? OR description LIKE ?) " );
+
         searchQuery.setParameter(1, "%"+querryString+"%");
+
         searchQuery.setParameter(2,"%"+querryString+"%");
+
         return searchQuery.getResultList();
+
     }
+
 }
+
