74f131e80d108d5181e6303361700bfbc13db523
basler <basler@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-04-12 02:21:28 +0000

    - Added lookup of latitude and longitude on yahoo site - Updated FileUploadBean to eliminate NullPointerExceptions on values that weren't entered - Add proxyHost and proxyPort back in for Yahoo service lookup - set address in FileUploadBean
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/controller/FileUploadBean.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/controller/FileUploadBean.java
@@ -24,8 +24,7 @@ import javax.persistence.PersistenceContext;
 import javax.transaction.UserTransaction;
 
 import javax.faces.context.FacesContext;
-//import javax.faces.event.PhaseEvent;
-//import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import javax.faces.context.ResponseWriter;
 import org.apache.shale.remoting.faces.ResponseFactory;
 
@@ -39,6 +38,9 @@ import com.sun.javaee.blueprints.components.ui.fileupload.FileUploadUtil;
 import com.sun.javaee.blueprints.petstore.search.IndexDocument;
 import com.sun.javaee.blueprints.petstore.search.Indexer;
 
+import com.sun.j2ee.blueprints.ui.geocoder.GeoCoder;
+import com.sun.j2ee.blueprints.ui.geocoder.GeoPoint;
+
 /**
  *
  * @author basler
@@ -49,6 +51,7 @@ public class FileUploadBean {
     @Resource UserTransaction utx;
     private boolean bDebug=false;
     private Logger _logger=null;
+    private static final String comma=", ";
 
     /**
      * <p>Factory for response writers that we can use to construct the
@@ -65,8 +68,11 @@ public class FileUploadBean {
         try {
             // set custom return enabled so Phaselistener knows not to send default response
             status.enableCustomReturn();
-            //FacesContext context=event.getFacesContext();
-            //HttpServletResponse response=(HttpServletResponse)context.getExternalContext().getResponse();
+            HttpServletResponse response=(HttpServletResponse)context.getExternalContext().getResponse();
+            
+            // get proxy host and port from servlet context
+            String proxyHost=context.getExternalContext().getInitParameter("proxyHost");
+            String proxyPort=context.getExternalContext().getInitParameter("proxyPort");
 
             // Acquire a response containing these results
             ResponseWriter writer = factory.getResponseWriter(context, "text/xml");
@@ -78,34 +84,109 @@ public class FileUploadBean {
                 Iterator iter = keySet.iterator();
                 while(iter.hasNext()){
                     String key = iter.next().toString();
-                    if(key.startsWith("fileLocation"))
+                    if(key.startsWith("fileLocation")) {
                         fileNameKey = key;
+                        break;
+                    }
                 }
-                String absoluteFileName = hmUpload.get(fileNameKey).toString();
+                String absoluteFileName=getStringValue(hmUpload, fileNameKey);                
                 System.out.println("Abs name: "+ absoluteFileName);
                 String fileName = null;
-                int lastSeparator = absoluteFileName.lastIndexOf("/") + 1;
-                if (lastSeparator != -1) {
-                    fileName = absoluteFileName.substring(lastSeparator, absoluteFileName.length());
+                if(absoluteFileName != null) {
+                    int lastSeparator = absoluteFileName.lastIndexOf("/") + 1;
+                    if (lastSeparator != -1) {
+                        fileName = absoluteFileName.substring(lastSeparator, absoluteFileName.length());
+                    }
                 }
                 
-                String compName = (String)hmUpload.get(FileUploadUtil.COMPONENT_NAME);
+                String compName=getStringValue(hmUpload, FileUploadUtil.COMPONENT_NAME);
+                
                 System.out.println("file name: "+ fileName);
                 Item item = new Item();
-                String prodId = hmUpload.get(compName+":product").toString();
-                String name = hmUpload.get(compName+":name").toString();
-                String desc = hmUpload.get(compName+":description").toString();
-                String price = hmUpload.get(compName+":price").toString();
+                String prodId=getStringValue(hmUpload, compName+":product");
+                String name=getStringValue(hmUpload, compName+":name");
+                String desc=getStringValue(hmUpload, compName+":description");
+                String price=getStringValue(hmUpload, compName+":price");
+                if(price.length() == 0) price="0";
                 Address addr = new Address();
-                //TO-DO: Add address fields to the file upload page and extract data
-                addr.setStreet1("street1");
-                addr.setStreet2("street2");
-                addr.setCity("city");
-                addr.setState("state");
-                addr.setZip("9999");
-                addr.setLatitude(-8.343545645);
-                addr.setLongitude(-9.76878787);
                 
+                // Add address fields to the file upload page and extract data
+                StringBuffer addressx=new StringBuffer();
+                String tmpx=getStringValue(hmUpload, compName+":street1");
+                if(tmpx.length() > 0) {
+                    addressx.append(tmpx);
+                    addr.setStreet1(tmpx);
+                } else {
+                    addr.setStreet1("");
+                }
+                
+                tmpx=getStringValue(hmUpload, compName+":street2");                
+                if(tmpx != null && tmpx.length() > 0) {
+                    addressx.append(" ");
+                    addressx.append(tmpx);
+                    addr.setStreet2(tmpx);
+                } else {
+                    addr.setStreet2("");
+                }
+                
+                tmpx=getStringValue(hmUpload, compName+":city");                
+                if(tmpx != null && tmpx.length() > 0) {
+                    addressx.append(comma);
+                    addressx.append(tmpx);
+                    addr.setCity(tmpx);
+                } else {
+                    addr.setCity("");
+                }
+                
+                tmpx=getStringValue(hmUpload, compName+":state");                
+                if(tmpx != null && tmpx.length() > 0) {
+                    addressx.append(comma);
+                    addressx.append(tmpx);
+                    addr.setState(tmpx);
+                } else {
+                    addr.setState("");
+                }
+                
+                tmpx=getStringValue(hmUpload, compName+":zip");                
+                if(tmpx != null && tmpx.length() > 0) {
+                    addressx.append(comma);
+                    addressx.append(tmpx);
+                    addr.setZip(tmpx);
+                } else {
+                    addr.setZip("");
+                }
+                
+                // get latitude & longitude
+                GeoCoder geoCoder=new GeoCoder();
+                if(proxyHost != null && proxyPort != null) {
+                    // set proxy host and port if it exists
+                    // NOTE: This requires write permissions for java.util.PropertyPermission to be granted
+                    getLogger().log(Level.INFO, "Setting proxy to " + proxyHost + ":" + proxyPort + ".  Make sure server.policy is updated to allow setting System Properties");
+                    geoCoder.setProxyHost(proxyHost);
+                    try {
+                        geoCoder.setProxyPort(Integer.parseInt(proxyPort));
+                    } catch (Exception ee) {
+                        ee.printStackTrace();
+                    }
+                } else {
+                    getLogger().log(Level.INFO, "A \"proxyHost\" and \"proxyPort\" isn't set as a web.xml context-param. A proxy server may be necessary to reach the open internet.");
+                }
+                
+                // use component to get points based on location (this uses Yahoo's map service
+                String totAddr=addressx.toString();
+                if(totAddr.length() > 0) {
+                    GeoPoint points[]=geoCoder.geoCode(totAddr);
+                    if ((points == null) || (points.length < 1)) {
+                        getLogger().log(Level.INFO, "No addresses for location - " + totAddr);
+                    } else if (points.length > 1) {
+                        getLogger().log(Level.INFO, "Matched " + points.length + " locations, taking the first one");
+                    }
+
+                    // set values to used for map location
+                    addr.setLatitude(points[0].getLatitude());
+                    addr.setLongitude(points[0].getLongitude());
+                }
+                    
                 SellerContactInfo contactInfo = new SellerContactInfo();
                 //TO-DO: Add SellerContactInfo fields to the file upload page and extract data
                 contactInfo.setFirstName("duke");
@@ -114,7 +195,14 @@ public class FileUploadBean {
                 item.setProductID(prodId);
                 item.setDescription(desc);
                 item.setName(name);
-                item.setPrice(new Float(price));
+                Float priceF;
+                try {
+                    priceF=new Float(price);
+                } catch (NumberFormatException nf) {
+                    priceF=new Float(0);
+                    getLogger().log(Level.INFO, "Price isn't in a proper number - " + price);
+                }
+                item.setPrice(priceF);
                 item.setImageURL(fileName);
                 item.setAddress(addr);
                 item.setContactInfo(contactInfo);
@@ -146,8 +234,10 @@ public class FileUploadBean {
                 }
             }
             StringBuffer sb=new StringBuffer();
-            //response.setContentType("text/xml;charset=UTF-8");
-            //response.setHeader("Cache-Control", "no-cache");
+            response.setContentType("text/xml;charset=UTF-8");
+            response.setHeader("Pragma", "No-Cache");
+            response.setHeader("Cache-Control", "no-cache,no-store,max-age=0");
+            response.setDateHeader("Expires", 1);            
             sb.append("<response>");
             sb.append("<message>***CUSTOM SERVER-SIDE RETURN *** MESSAGE->");
             sb.append(status.getMessage());
@@ -182,7 +272,7 @@ public class FileUploadBean {
     }
     
     
-    public void indexItem(IndexDocument indexDoc) {
+    private void indexItem(IndexDocument indexDoc) {
         // Add document to index
         Indexer indexer=null;
         try {
@@ -201,6 +291,16 @@ public class FileUploadBean {
             }
         }
     }
+    
+    
+    private String getStringValue(HashMap hmUpload, String key)  {
+        String sxTemp=(String)hmUpload.get(key);
+        if(sxTemp == null) {
+            sxTemp="";
+        }
+        return sxTemp;
+    }
+    
     
     /**
     * Method getLogger
