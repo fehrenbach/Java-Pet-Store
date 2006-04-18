c1dd03c9599520a13fd72fd2f069ae4087a4a0d7
smitha <smitha@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-04-18 22:00:43 +0000

    addItem returns item ID

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/CatalogFacade.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/CatalogFacade.java
@@ -10,24 +10,24 @@ import javax.transaction.*;
 import javax.annotation.*;
 
 public class CatalogFacade implements ServletContextListener {
-
-   //@PersistenceContext(name="bppu")
-   //private EntityManager em;
+    
+    //@PersistenceContext(name="bppu")
+    //private EntityManager em;
     @PersistenceUnit(unitName="bppu") private EntityManagerFactory emf;
     @Resource UserTransaction utx;
-
+    
     public CatalogFacade(){ }
-
+    
     public void contextDestroyed(ServletContextEvent sce) {
-       //close the factory and all entity managers associated with it
-       if (emf.isOpen()) emf.close();
+        //close the factory and all entity managers associated with it
+        if (emf.isOpen()) emf.close();
     }
     
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext context = sce.getServletContext();
         context.setAttribute("CatalogFacade", this);
     }
-
+    
     public Collection getCategories(){
         EntityManager em = emf.createEntityManager();
         Collection results = em.createNativeQuery("SELECT * FROM Category").getResultList();
@@ -42,7 +42,7 @@ public class CatalogFacade implements ServletContextListener {
         em.close();
         return results;
     }
-
+    
     /**
      * Value List Handler for items. Uses the Java Persistence query language.
      * @param pID is the product id that the item belongs to
@@ -50,24 +50,24 @@ public class CatalogFacade implements ServletContextListener {
      * @param chunkSize the maximum number of results to retrieve
      * @returns a List of Item objects
      */
-    public List<Item> getItemsVLH(String pID, int start, int chunkSize){    
-       EntityManager em = emf.createEntityManager();
-
-       //make Java Persistence query
-       System.out.println("CatalogFacade: productId=" + pID + " start=" + start + " chunkSize=" + chunkSize);
-       
-       //Query query = em.createNamedQuery("Item.getItemsPerProductCategory");
-       Query query = em.createQuery("SELECT i FROM Item i WHERE i.productID = :pID");
-       List<Item>  items = query.setParameter("pID",pID).setFirstResult(start).setMaxResults(chunkSize).getResultList();      
+    public List<Item> getItemsVLH(String pID, int start, int chunkSize){
+        EntityManager em = emf.createEntityManager();
+        
+        //make Java Persistence query
+        System.out.println("CatalogFacade: productId=" + pID + " start=" + start + " chunkSize=" + chunkSize);
+        
+        //Query query = em.createNamedQuery("Item.getItemsPerProductCategory");
+        Query query = em.createQuery("SELECT i FROM Item i WHERE i.productID = :pID");
+        List<Item>  items = query.setParameter("pID",pID).setFirstResult(start).setMaxResults(chunkSize).getResultList();
        /*
-       Iterator<Item> it = items.iterator();            
+       Iterator<Item> it = items.iterator();
           while (it.hasNext()) {
                Item i = it.next();
               System.out.println("*****CatalogFacade:**" +i.getItemID() + "\n");
           }
         **/
-       em.close();
-       return items;
+        em.close();
+        return items;
     }
     /**
      * Value List Handler for items. Found by location radius for google map
@@ -77,41 +77,41 @@ public class CatalogFacade implements ServletContextListener {
      * @param chunkSize the maximum number of results to retrieve
      * @returns a List of Item objects
      */
-    public List<Item> getItemsByRadius(String categoryID, int start, int chunkSize, 
-                        double fromLatitude, double toLatitude, double fromLongitude, double toLongitude){    
-       EntityManager em = emf.createEntityManager();
-    
-       //System.out.println("CatalogFacade.getItemsByRadius: categoryID=" + categoryID + " start=" + start + " chunkSize=" + chunkSize);
-       //select i.itemID from item i , product p where i.productID= p.productID  AND p.categoryID = 'CATS'
-       Query query = em.createQuery("SELECT i FROM Item i, Product p WHERE i.productID=p.productID AND p.categoryID = :categoryID");
-       List<Item>  items = query.setParameter("categoryID",categoryID).setFirstResult(start).setMaxResults(chunkSize).getResultList();      
-       
-        /*Iterator<Item> it = items.iterator();            
+    public List<Item> getItemsByRadius(String categoryID, int start, int chunkSize,
+            double fromLatitude, double toLatitude, double fromLongitude, double toLongitude){
+        EntityManager em = emf.createEntityManager();
+        
+        //System.out.println("CatalogFacade.getItemsByRadius: categoryID=" + categoryID + " start=" + start + " chunkSize=" + chunkSize);
+        //select i.itemID from item i , product p where i.productID= p.productID  AND p.categoryID = 'CATS'
+        Query query = em.createQuery("SELECT i FROM Item i, Product p WHERE i.productID=p.productID AND p.categoryID = :categoryID");
+        List<Item>  items = query.setParameter("categoryID",categoryID).setFirstResult(start).setMaxResults(chunkSize).getResultList();
+        
+        /*Iterator<Item> it = items.iterator();
           while (it.hasNext()) {
                Item i = it.next();
               System.out.println("*****CatalogFacade:**" +i.getItemID() + "\n");
           }
          **/
-       
-       em.close();
-       return items;
+        
+        em.close();
+        return items;
     }
     
     /**
      * Gets a list of all the zipcode/city/state for autocomplete on user forms
-     * Need to enhance so that returned list is cached for reuse at application scope 
+     * Need to enhance so that returned list is cached for reuse at application scope
      * and held as member field of facade.
      * @returns a List of ZipLocation objects
      */
-    public List<ZipLocation> getZipCodeLocations(){  
-       EntityManager em = emf.createEntityManager();
-       //Query query = em.createQuery("SELECT  z FROM ZipLocation z");
-       Query query = em.createNamedQuery("getAllZipCityState");
-       List<ZipLocation>  zipCodeLocations = query.getResultList();    
-       em.close();
-       return zipCodeLocations;
+    public List<ZipLocation> getZipCodeLocations(){
+        EntityManager em = emf.createEntityManager();
+        //Query query = em.createQuery("SELECT  z FROM ZipLocation z");
+        Query query = em.createNamedQuery("getAllZipCityState");
+        List<ZipLocation>  zipCodeLocations = query.getResultList();
+        em.close();
+        return zipCodeLocations;
     }
-
+    
     public Collection getProducts(String catID){
         EntityManager em = emf.createEntityManager();
         Collection results = em.createNativeQuery("SELECT * FROM Product WHERE categoryID LIKE ?")
@@ -119,22 +119,22 @@ public class CatalogFacade implements ServletContextListener {
         em.close();
         return results;
     }
-
+    
     public Collection getItems(String productID){
         EntityManager em = emf.createEntityManager();
         Collection results = em.createNativeQuery("SELECT * FROM Item WHERE productid LIKE ?")
-                .setParameter(1, "%"+productID+"%").getResultList();
+        .setParameter(1, "%"+productID+"%").getResultList();
         em.close();
         return results;
     }
-
+    
     public Category getCategory(String categoryID){
         EntityManager em = emf.createEntityManager();
         Category result = em.find(Category.class,categoryID);
         em.close();
         return result;
     }
-
+    
     public Item getItem(String itemID){
         EntityManager em = emf.createEntityManager();
         Item result = em.find(Item.class,itemID);
@@ -142,7 +142,7 @@ public class CatalogFacade implements ServletContextListener {
         return result;
     }
     
-    public void addItem(Item item){
+    public String addItem(Item item){
         EntityManager em = emf.createEntityManager();
         try{
             utx.begin();
@@ -157,8 +157,9 @@ public class CatalogFacade implements ServletContextListener {
         } finally {
             em.close();
         }
+        return item.getItemID();
     }
-
+    
     public Collection doSearch(String querryString){
         EntityManager em = emf.createEntityManager();
         Query searchQuery = em.createNativeQuery("SELECT * FROM Item WHERE (name LIKE ? OR description LIKE ?) " );
