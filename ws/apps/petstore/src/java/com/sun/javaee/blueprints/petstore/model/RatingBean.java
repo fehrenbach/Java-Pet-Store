19ecb28c61347da91feabd97d3906ddaec8e46f0
basler <basler@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-04-21 03:32:55 +0000

    Added place for persistent code to go.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/RatingBean.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/RatingBean.java
@@ -15,8 +15,8 @@ import java.util.logging.Logger;
  */
 public class RatingBean {
     
-    private int grade=0, grade2=0, grade3=0;
-    private Logger logger=Logger.getLogger("helpme");
+    private String itemId=null;
+    private int grade=0;
     
     /** Creates a new instance of RatingBean */
     public RatingBean() {
@@ -25,33 +25,27 @@ public class RatingBean {
     public String[] getRatingText() {
         return new String[]{"Hate It", "Below Average", "Average", "Above Average", "Love It"};
     }
-    
-    
+
     public void setGrade(int grade) {
-        logger.info("\n**** Setting Grade = " + grade);
-        this.grade=grade;
+        // "itemId" is the primarykey for the product...
+        // "grade" is the grade to be store
+        String itemId=getItemId();
+        if(itemId != null) {
+            // persistant code goes here !!!!
+        }
+        
     }
+
     public int getGrade() {
-        logger.info("\n*** Getting Grade = " + grade);
         return grade;
     }
     
-    public void setGrade2(int grade) {
-        logger.info("\n**** Setting Grade2 = " + grade);
-        this.grade2=grade;
-    }
-    public int getGrade2() {
-        logger.info("\n*** Getting Grade2 = " + grade2);
-        return grade2;
+
+    public void setItemId(String itemId) {
+        this.itemId=itemId;
     }
     
-    public void setGrade3(int grade) {
-        logger.info("\n**** Setting Grade3 = " + grade);
-        this.grade3=grade;
+    public String getItemId() {
+        return itemId;
     }
-    public int getGrade3() {
-        logger.info("\n*** Getting Grade3 = " + grade3);
-        return grade3;
-    }
-    
 }
