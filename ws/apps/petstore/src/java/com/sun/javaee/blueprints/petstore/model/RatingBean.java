cfecaebb2b98f376675855ff395e23106e15d681
smitha <smitha@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-05-04 01:08:22 +0000

    check for "Not Interested"

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/RatingBean.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/RatingBean.java
@@ -1,5 +1,5 @@
 /* Copyright 2006 Sun Microsystems, Inc. All rights reserved. You may not modify, use, reproduce, or distribute this software except in compliance with the terms of the License at: http://developer.sun.com/berkeley_license.html
-$Id: RatingBean.java,v 1.6 2006-05-03 21:48:59 inder Exp $ */
+$Id: RatingBean.java,v 1.7 2006-05-04 01:08:22 smitha Exp $ */
 
 package com.sun.javaee.blueprints.petstore.model;
 import java.util.logging.Logger;
@@ -20,31 +20,33 @@ public class RatingBean {
     /** Creates a new instance of RatingBean */
     public RatingBean() {
         FacesContext context=FacesContext.getCurrentInstance();
-        Map<String,Object> contextMap=context.getExternalContext().getApplicationMap();        
+        Map<String,Object> contextMap=context.getExternalContext().getApplicationMap();
         cf=(CatalogFacade)contextMap.get("CatalogFacade");
     }
     
     public String[] getRatingText() {
         return new String[]{"Hate It", "Below Average", "Average", "Above Average", "Love It"};
     }
-
+    
     public void setGrade(int grade) {
         // "itemId" is the primarykey for the product...
         // "grade" is the grade to be store
         String itemId=getItemId();
         System.out.println("\n *** have rating - " + itemId + " - " + grade);
         if(itemId != null) {
-            // persistant code goes here !!!!
-            Item item = cf.getItem(itemId);
-            item.addRating(grade);
-            cf.updateRating(item);
+            if(grade>=1){
+                //call the catalog facade to update rating
+                Item item = cf.getItem(itemId);
+                item.addRating(grade);
+                cf.updateRating(item);
+            }
         }
     }
-
+    
     public int getGrade() {
         return grade;
-    }  
-
+    }
+    
     public void setItemId(String itemId) {
         this.itemId=itemId;
     }
