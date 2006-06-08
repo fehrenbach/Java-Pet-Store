71b3e8fb83250be91fd7b4536f62fe3873a4cadc
basler <basler@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2006-06-08 22:57:49 +0000

    Fixed paypal currency problem from depricated type.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/PayPalBean.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/model/PayPalBean.java
@@ -1,5 +1,5 @@
 /* Copyright 2006 Sun Microsystems, Inc. All rights reserved. You may not modify, use, reproduce, or distribute this software except in compliance with the terms of the License at: http://developer.sun.com/berkeley_license.html
-$Id: PayPalBean.java,v 1.4 2006-05-05 20:15:24 inder Exp $ */
+$Id: PayPalBean.java,v 1.5 2006-06-08 22:57:49 basler Exp $ */
 
 package com.sun.javaee.blueprints.petstore.model;
 
@@ -16,7 +16,6 @@ public class PayPalBean {
     /** Creates a new instance of PayPalBean */
     public PayPalBean() {
         postData=new BuyNowPostData();
-        postData.setCurrencyCode("U.S. Dollar");
         postData.setShippingCost(new BigDecimal("25"));
         postData.setTax(new BigDecimal("10"));
         postData.setUndefinedQuantity("1");
