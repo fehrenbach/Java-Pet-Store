71d564dd13469d6b43e53a66b80ac787375eaf1e
yutayoshida <yutayoshida@cd402d93-e0c7-e09c-d6c0-c6a20e4839a2>
2007-01-10 19:21:03 +0000

    Fix: Issue[14] wrong params for AffineTransform.getScaleInstace().

--- ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/util/ImageScaler.java
+++ ws/apps/petstore/src/java/com/sun/javaee/blueprints/petstore/util/ImageScaler.java
@@ -1,5 +1,5 @@
 /* Copyright 2006 Sun Microsystems, Inc. All rights reserved. You may not modify, use, reproduce, or distribute this software except in compliance with the terms of the License at: http://developer.sun.com/berkeley_license.html
-$Id: ImageScaler.java,v 1.4 2007-01-06 00:09:30 inder Exp $ */
+$Id: ImageScaler.java,v 1.5 2007-01-10 19:21:03 yutayoshida Exp $ */
 
 package com.sun.javaee.blueprints.petstore.util;
 
@@ -92,7 +92,7 @@ public class ImageScaler {
         BufferedImage th = new BufferedImage((int)(w*power), (int)(h*power), image.getType());
         double powerW = ((double) thumbWidth) / w;
         double powerH = ((double) thumbHeight) / h;
-        AffineTransformOp op = new AffineTransformOp(AffineTransform.getScaleInstance(power, power),
+        AffineTransformOp op = new AffineTransformOp(AffineTransform.getScaleInstance(powerW, powerH),
                 AffineTransformOp.TYPE_BILINEAR);
         op.filter(image, th);
         ImageIO.write(th, format, new File(to));
